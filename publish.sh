6d384dbf1c2914bb3e211a3a6c521fdd9e157e33
Federico Brigante <me@fregante.com>
2023-05-02 01:38:52 +0800

    Update publish workflow

---
+++ publish.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+npm publish || true # Try publishing the version in git, in case it was manually changed
+if npm version "$(npm info webextension-polyfill version)"; then
+	npm publish
+fi
